677fe0eaa7912e88b3e59c6d444057a1e36e4bbd
rubylan <ruby_lan_123@163.com>
2025-08-12 19:27:50 +0800

    Fix code exec bug for single-line; Debug & scripts; Add global step and more monitoring metrics in reward func.

---
+++ scripts/mm_retool_lxh/0702/all_in_one.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+eval "$('/mnt/dolphinfs/hdd_pool/docker/user/hadoop-basecv/lanxiaohan/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+conda activate mm-eureka
+echo "conda activate mm-eureka"
+
+EXP_ROOT=/mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/lanxiaohan/reasoning_hc/code/MM-EUREKA
+cd $EXP_ROOT
+
+JOB_ARGS=($(python get_job_args_lc.py))
+echo "JOB_ARGS: ${JOB_ARGS[@]}"
+
+NNODES="${JOB_ARGS[0]}"
+GPUS_PER_NODE="${JOB_ARGS[1]}"
+MASTER_ADDR="${JOB_ARGS[2]}"
+MASTER_PORT="${JOB_ARGS[3]}"
+NODE_RANK="${JOB_ARGS[4]}"
+
+echo "NNODES: ${NNODES}"
+echo "GPUS_PER_NODE: ${GPUS_PER_NODE}"
+echo "MASTER_ADDR: ${MASTER_ADDR}"
+echo "MASTER_PORT: ${MASTER_PORT}"
+echo "NODE_RANK: ${NODE_RANK}"
+# 从命令行参数获取NODE_RANK，如果没有则从环境变量获取
+if [ $# -gt 0 ]; then
+    NODE_RANK=$1
+else
+    NODE_RANK=${NODE_RANK:-0}
+fi
+
+echo "当前NODE_RANK值: $NODE_RANK"
+
+case $NODE_RANK in
+    0)
+        echo "执行NODE_RANK=0的任务..."
+        # python3 /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/qiuhaibo/workspace/tools/scripts/gpu_util.py
+        # bash /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/lanxiaohan/reasoning_hc/code/MM-EUREKA/scripts/mm_retool_lxh/0702/retool_run_0702_bs128_reward_v4.sh
+        ;;
+    1)
+        echo "执行NODE_RANK=1的任务..."
+        # python3 /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/qiuhaibo/workspace/tools/scripts/gpu_util.py
+        bash /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/lanxiaohan/reasoning_hc/code/MM-EUREKA/scripts/mm_retool_lxh/0702/retool_run_0702_bs64_reward_v5_mixed_sft_k12_rl.sh
+        # bash /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/lanxiaohan/reasoning_hc/code/MM-EUREKA/scripts/mm_retool_lxh/0702/retool_run_0702_bs128_reward_v4.sh
+        ;;
+    2)
+        echo "执行NODE_RANK=2的任务..."
+        bash /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/lanxiaohan/reasoning_hc/code/MM-EUREKA/scripts/mm_retool_lxh/0702/retool_run_0702_bs64_reward_v4.sh
+        ;;
+    3)
+        echo "执行NODE_RANK=3的任务..."
+        bash /mnt/dolphinfs/ssd_pool/docker/user/hadoop-basecv/lanxiaohan/reasoning_hc/code/MM-EUREKA/scripts/mm_retool_lxh/0702/retool_run_0702_bs64_reward_v5.sh
+        ;;
+    *)
+        echo "错误: 不支持的NODE_RANK值: $NODE_RANK"
+        echo "用法: $0 [1|2|3|4] 或设置环境变量 NODE_RANK"
+        exit 1
+        ;;
+esac
+
+echo "NODE_RANK=$NODE_RANK 的任务执行完成"
